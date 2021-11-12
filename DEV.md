ae3292cc6e487b1fd47cfd6ab181c9cddc1b8028
Timothy Cheng <timothycheng@Timothys-MacBook-Pro.local>
2021-11-12 12:35:28 -0500

    posts re now saved in memory. Would be wiped when program is reloaded

--- DEV.md
+++ DEV.md
@@ -1,11 +1,24 @@
 # Development Notes
 
+
+## FastAPI
+
+Start server
+```
+uvicorn main:app
+```
+`main` is the file and `app` is the FastAPI instance.
+
+Add option to reload when file is modified. This way we don't need to restart the server manually every time after a change.
+```
+uvicorn main:app --reload
+```
 ## Virtual Environment
 
 Create a virtual environment:
 
 ```
- python3 -m venv venv
+python3 -m venv venv
 ```
 
 Second venv is the virtual environment name
@@ -14,4 +27,9 @@ Enable venv on command line:
 
 ```
 Source venv/bin/activate
+```
+
+Install libraries specified in requirements.txt
+```
+pip install -r /path/to/requirements.txt
 ```
--- main.py
+++ main.py
@@ -1,19 +1,38 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from random import randint
+
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+ 
+my_posts = [{"title":"title1", "content": "beach", "id": 1}, 
+{"title":"Europe", "content": "europe is fun!", "id":2}]
 
 @app.get("/")
 def root():
     return {"message": "Welcome to my API!"}
 
-
+# best practice: endpoints should be plural 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your post."}
+    return {"data": my_posts}
 
+@app.get("/posts/{id}")
+def get_post():
+    return
 
 @app.post("/posts")
-def create_posts(payload: dict = Body(...)):
+def create_posts(post: Post):
+    post_dict = post.dict()
+    new_id = abs(hash(post_dict['title']+post_dict['content']+str(randint(1,1000))))
+    post_dict['id'] = new_id
 
-    return {"message": "successfully created post."}
+    my_posts.append(post_dict)
+    return {"content":post_dict}
